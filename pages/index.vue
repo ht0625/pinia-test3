a4283334d7ada3c65cb3e196171be79a4abe83b1
Tamura <67675234+ht0625@users.noreply.github.com>
2024-11-30 11:14:01 +0900

    pinia導入動かない状態であるが一旦コミット

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const counter = useCounterStore();
+</script>
+
+<template>
+  <v-container>
+    <nuxt-link to="/count">count ページへ飛ぶ </nuxt-link> //countページに遷移する
+    <p>ボタンを{{ counter.count }}回おしました!</p>
+  </v-container>
+</template>
